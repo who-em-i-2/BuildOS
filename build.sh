ae3efbf0758e11b39954c3e91e89847911ee98e9
ImPrashantt <prashant33968@gmail.com>
2023-03-10 04:07:59 +0000

    start | V

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch superior_lavender-userdebug
+
+# 0 = Vanilla or Gapps
+# 1 = Vanilla and Gapps
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
+exp_gapps() {
+export BUILD_WITH_GAPPS=true
+}
+
 compile_plox () {
-mka bacon -j10
+m bacon -j10
 }
