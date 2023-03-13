e485df75b966376e2a21f0783e8f833117ca84fe
ImSpiDy <SpiDy2713@gmail.com>
2023-03-13 18:27:56 +0530

    CR Android 10 | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Normal build steps
 . build/envsetup.sh
-lunch havoc_lavender-userdebug
+lunch lineage_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 #export ARROW_GAPPS=true
 
 compile_plox () {
-mka bacon -j$(nproc --all)
+mka bacon -j17
 }
