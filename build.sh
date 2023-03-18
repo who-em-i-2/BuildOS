19331c34f56bee09244673ad4924d771076d8a09
ImSpiDy <SpiDy2713@gmail.com>
2023-03-18 12:44:39 +0530

    Nusa Q | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Normal build steps
 . build/envsetup.sh
-lunch rr_lavender-userdebug
+lunch nad_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-#export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 #export ARROW_GAPPS=true
 
 compile_plox () {
-mka bacon -j17
+make nad -j17
 }
