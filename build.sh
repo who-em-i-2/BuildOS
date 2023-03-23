971d1ce1607e4ce96c99a293b13bedf0e09715e5
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-03-23 22:20:08 +0530

    ci: topaz moment | C
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch aospa_lavender-user
 
-build_gapps=1
+build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export USE_GAPPS=false
 
 exp_gapps() {
@@ -14,5 +14,7 @@ export USE_GAPPS=true
 }
 
 compile_plox () {
-make nad -j17
+#ls /tmp/rom/out/target/product/lavender/boot.img || make bootimage -j12
+./rom-build.sh lavender -t user -s keys -j16
+ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
