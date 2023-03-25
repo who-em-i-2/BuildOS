5743e7d0fe26df09802e84656736957a8689a282
ImSpiDy <SpiDy2713@gmail.com>
2023-03-25 00:10:23 +0000

    RDX | R

--- build.sh
+++ build.sh
@@ -1,18 +1,10 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-userdebug
-
-build_gapps=1
+lunch lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
-export USE_GAPPS=false
-
-exp_gapps() {
-export USE_GAPPS=true
-}
 
 compile_plox () {
-make nad -j17
+make reloaded -j17
 }
