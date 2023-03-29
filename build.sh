05e6b50340930fbf74e1f9dace96335144d59eaf
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-03-29 05:56:59 +0530

    less go 4.19 | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,25 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lavender-userdebug
+lunch bliss_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TARGET_BOOTANIMATION_RES=1080
+
+build_gapps=1
+export BLISS_BUILD_VARIANT=vanilla
+export ARROW_GAPPS=false
+export WITH_GMS=false
+export WITH_GAPPS=false
+
+exp_gapps() {
+export BLISS_BUILD_VARIANT=gapps
+export ARROW_GAPPS=true
+export WITH_GMS=true
+export WITH_GAPPS=true
+}
 
 compile_plox () {
-make reloaded -j17
+make blissify -j16
 }
