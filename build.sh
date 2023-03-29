d015643b4dab554555b28353d2acbef1c994c9bc
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-03-30 01:12:00 +0530

    ci: 4.19 moment | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -8,16 +8,15 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export TARGET_BOOTANIMATION_RES=1080
 
 build_gapps=1
-export BLISS_BUILD_VARIANT=vanilla
 export ARROW_GAPPS=false
 export WITH_GMS=false
 export WITH_GAPPS=false
 
 exp_gapps() {
-export BLISS_BUILD_VARIANT=gapps
 export ARROW_GAPPS=true
 export WITH_GMS=true
 export WITH_GAPPS=true
+export BLISS_BUILD_VARIANT=gapps
 }
 
 compile_plox () {
