c089bae60777f9f3eb8d21c34eb4c90dd8e4a138
ImSpiDy <SpiDy2713@gmail.com>
2023-04-04 08:55:46 +0000

    Bonk Gaypps | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,16 +2,9 @@
 . build/envsetup.sh
 lunch nad_lavender-userdebug
 
-build_gapps=1
-
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export USE_GAPPS=false
-
-exp_gapps() {
-export USE_GAPPS=true
-}
 
 compile_plox () {
 make nad -j17
