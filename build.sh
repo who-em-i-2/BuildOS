6069de3ba5b646e4210ef54fed8caabde96f7424
ImSpiDy <SpiDy2713@gmail.com>
2023-05-08 08:45:39 +0000

    final | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 # Normal build steps
 . build/envsetup.sh
-. /vendor/rr/build/envsetup.sh
 lunch rr_lavender-userdebug
 
 # export variable here
@@ -8,11 +7,10 @@ export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 build_gapps=1
-export GAPPS=""
+export GAPPS=false
 
 exp_gapps() {
 export GAPPS=true
-export GAPPS_ARM32=false
 }
 
 compile_plox () {
