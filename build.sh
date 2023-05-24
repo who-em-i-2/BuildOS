f3f3519e532b23254a33e053487a22cc43eb672b
ImSpiDy <SpiDy2713@gmail.com>
2023-05-24 20:51:39 +0000

    build nad | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ lunch nad_lavender-userdebug
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
+build_gapps=1
+
+exp_gapps () {
+export USE_GAPPS=true
+}
+
 compile_plox () {
 make nad -j17
 }
