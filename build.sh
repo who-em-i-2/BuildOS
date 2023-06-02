095280e9e2e63124c37ada29eb4865753c0109d9
ImSpiDy <SpiDy2713@gmail.com>
2023-06-02 03:00:37 +0000

    CR-A11
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch lineage_lavender-userdebug
+
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-
-build_gapps=0
+export WITH_GMS=true
 
 exp_gapps () {
-export USE_GAPPS=true
+export WITH_GMS=false
 }
 
 compile_plox () {
-make nad -j17
+mka bacon -j17
 }
