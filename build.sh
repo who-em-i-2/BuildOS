ff2f08f55e81d04ac8f0cb26baf228d273019543
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-06-26 19:43:23 +0530

    less go | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -15,6 +15,6 @@ export USE_GAPPS=true
 
 compile_plox () {
 #ls /tmp/rom/out/target/product/lavender/boot.img || make bootimage -j12
-./rom-build.sh lavender -t user -s keys -j12
+./rom-build.sh lavender -t user -s keys -j16
 ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
