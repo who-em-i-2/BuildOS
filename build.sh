be8d045f86db6aeb6645e60cfc68060ff6b73862
ImSpiDy <SpiDy2713@gmail.com>
2023-07-11 04:07:06 +0530

    Leaf Again
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ build_gapps=0
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false
 
 compile_plox () {
 m leaf -j17
