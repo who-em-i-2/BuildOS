d1824dc0918bc9dcd840efe3f6e96c31910e2e6b
ImSpiDy <SpiDy2713@gmail.com>
2023-07-11 03:58:58 +0530

    Legionnnn
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lavender-userdebug
+lunch legion_lavender-userdebug
 
-build_gapps=0
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false
+export LEGION_GAPPS=false
+
+exp_gapps() {
+export LEGION_GAPPS=true
+}
 
 compile_plox () {
-m leaf -j17
+m bacon -j17
 }
