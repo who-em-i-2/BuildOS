4af513ed79ded243bdc361e17f27715198309e3b
ImSpiDy <SpiDy2713@gmail.com>
2023-07-11 02:10:50 +0530

    Go Leaf Go
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,19 +1,13 @@
 # Normal build steps
 . build/envsetup.sh
-lunch havoc_lavender-userdebug
+lunch lavender-userdebug
 
 build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GMS=true
-
-exp_gapps () {
-export WITH_GMS=false
-}
 
 compile_plox () {
-m bacon -j17
-#ls /tmp/rom/out/target/product/lav*/*2023*zip || make bacon -j8
+m leaf -j17
 }
