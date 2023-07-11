68253ecec06ec7b3195fd777d8b9d05b1617bbaa
ImSpiDy <SpiDy2713@gmail.com>
2023-07-12 04:40:03 +0530

    One More | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 . build/envsetup.sh
 lunch lavender-userdebug
 
-build_gapps=0
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=true
+
+exp_gapps() {
+export WITH_GMS=false
+}
 
 compile_plox () {
 m leaf -j17
