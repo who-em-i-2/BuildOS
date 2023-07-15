662d9178d3f35b2d408d7c3e0df07a25b200159e
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-07-15 08:47:31 +0530

    switch to erofs | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ lunch awaken_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_DUP_RULES=true
+#export BUILD_BROKEN_DUP_RULES=true
 export RELAX_USES_LIBRARY_CHECK=true
 #export TARGET_BOOTANIMATION_RES=1080
 
