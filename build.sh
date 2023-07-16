0e911d12b6514fb08d3c61feb6330867d15c7ddc
ImSpiDy <SpiDy2713@gmail.com>
2023-07-16 08:10:10 +0000

    Final | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 . build/envsetup.sh
 lunch havoc_lavender-userdebug
 
-build_gapps=0
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GMS=true
+export WITH_GAPPS=false
 
 exp_gapps () {
-export WITH_GMS=false
+export WITH_GAPPS=true
 }
 
 compile_plox () {
