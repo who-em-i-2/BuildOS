9898601d5f6111e5a107b75847227e291c96c086
ImSpiDy <SpiDy2713@gmail.com>
2023-07-21 01:11:17 +0530

    Banilla | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
 
-build_gapps=1
+build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GMS=true
+#export WITH_GMS=true
 
 exp_gapps () {
 export WITH_GMS=true
