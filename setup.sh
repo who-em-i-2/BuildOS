4bac5199441eb0e3de363e43a736948bd5604b82
NotZeetaa <rodrigo2005contente@gmail.com>
2023-07-21 07:34:25 +0000

    Revert "setup: Build with LTO"
    This reverts commit b951cf305574a9bf8a6f4686ca51a864fa65bf03.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250
 
 # Build
 cd kernel
-bash build.sh null alioth lto
-bash build.sh null munch lto
-bash build.sh null lmi lto
-bash build.sh null apollo lto
+bash build.sh null alioth null
+bash build.sh null munch null
+bash build.sh null lmi null
+bash build.sh null apollo null
