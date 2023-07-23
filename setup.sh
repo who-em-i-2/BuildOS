9859d1ab4f95dff21f455cf4e20492276191b43e
NotZeetaa <rodrigo2005contente@gmail.com>
2023-07-23 17:52:03 +0000

    setup: Switch to PA branch
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b walt kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b PA kernel
 
 # Build
 cd kernel
