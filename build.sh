dff79b4b19ac71cde6d4c6401e4c872b68f3966d
ImSpiDy <SpiDy2713@gmail.com>
2023-07-25 21:13:55 +0530

    GayX K4.19
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -7,11 +7,17 @@ build_gapps=0
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-#export WITH_GMS=false
+#export BUILD_BROKEN_DUP_RULES=true
+export RELAX_USES_LIBRARY_CHECK=true
+export WITH_GMS=true
 
 exp_gapps() {
+export TARGET_GAPPS_ARCH=arm64
+export USE_GAPPS=true
+export ARROW_GAPPS=true
 export WITH_GMS=true
-export TARGET_CORE_GMS=true
+export WITH_GAPPS=true
+export BLISS_BUILD_VARIANT=gapps
 }
 
 compile_plox () {
