dbe526b96e7cf15b706645a3522e6e29ef6bc4f4
ImSpiDy <SpiDy2713@gmail.com>
2023-07-26 07:11:29 +0530

    RiseOS 13
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -7,11 +7,17 @@ build_gapps=1
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+#export BUILD_BROKEN_DUP_RULES=true
+export RELAX_USES_LIBRARY_CHECK=true
 export WITH_GMS=false
 
 exp_gapps() {
+export USE_GAPPS=true
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
+export WITH_GAPPS=true
+export BLISS_BUILD_VARIANT=gapps
+export TARGET_USES_MINI_GAPPS=true
 }
 
 compile_plox () {
