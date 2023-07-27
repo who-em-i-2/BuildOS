41db677c057252c3d04129a9d2eac7e7acd69d32
ImSpiDy <SpiDy2713@gmail.com>
2023-07-28 00:25:12 +0530

    AlphaDroid
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
 
-#build_gapps=1
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
@@ -11,14 +11,15 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
 #export WITH_GMS=false
 
-#exp_gapps() {
+exp_gapps() {
 export USE_GAPPS=true
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
 export WITH_GAPPS=true
 export BLISS_BUILD_VARIANT=gapps
 export TARGET_USES_MINI_GAPPS=true
-#}
+export GAPPS_BUILD_TYPE=2
+}
 
 compile_plox () {
 mka bacon -j17
