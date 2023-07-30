4b2ff4e9eea0cd450909721e0fc0e9f7f619c21d
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-07-30 15:36:21 +0530

    PA 4.19 moment xd

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch aospa_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=true
 #export BUILD_BROKEN_DUP_RULES=true
-export RELAX_USES_LIBRARY_CHECK=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
+#export BUILD_BROKEN_DUP_RULES=true
+#export RELAX_USES_LIBRARY_CHECK=true
 #export TARGET_BOOTANIMATION_RES=1080
-export LINEAGE_BUILDTYPE=COMMUNITY
 
 #build_gapps=1
-export ARROW_GAPPS=false
-export WITH_GMS=false
-export WITH_GAPPS=false
-export USE_GAPPS=false
+#export ARROW_GAPPS=false
+#export WITH_GMS=false
+#export WITH_GAPPS=false
+#export USE_GAPPS=false
 
 exp_gapps() {
 export TARGET_GAPPS_ARCH=arm64
@@ -22,9 +23,11 @@ export USE_GAPPS=true
 export ARROW_GAPPS=true
 export WITH_GMS=true
 export WITH_GAPPS=true
-export BLISS_BUILD_VARIANT=gapps 
+export BLISS_BUILD_VARIANT=gapps
 }
 
 compile_plox () {
-make bacon -j16
+./rom-build.sh lavender -t userdebug -s keys -j16
+ls /tmp/rom/aospa*.zip || ./rom-build.sh lavender -t user -s keys -j16
+ls /tmp/rom/aospa*.zip && mv -f /tmp/rom/aospa*.zip /tmp/rom/out/target/product/lavender
 }
