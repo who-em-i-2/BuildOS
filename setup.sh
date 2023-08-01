ee2f270ac5f0a327bc1030d3baae9f68ffaef72f
NotZeetaa <rodrigo2005contente@gmail.com>
2023-08-01 08:15:35 +0000

    setup: Switch to miui branch
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b staging kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b miui kernel
 
 # Build
 cd kernel
