e8a3b27e7d11fedfe0dfbd112decb7556c0e1b83
NotZeetaa <rodrigo2005contente@gmail.com>
2023-08-08 23:33:57 +0000

    setup: Build alioth for now
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-echo "********************************"
-ldd --version
-echo "********************************"
-
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b latency kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b lineage kernel
 
 # Build
 cd kernel
 bash build.sh null alioth null
-bash build.sh null munch null
-bash build.sh null lmi null
-bash build.sh null apollo null
+
