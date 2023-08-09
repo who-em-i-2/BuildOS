9e55e5613126a3c7299806784c2f7083f542b993
ImSpiDy <SpiDy2713@gmail.com>
2023-08-10 02:30:55 +0530

    Lets Go | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-userdebug
+lunch nad_lavender-user
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export USE_GAPPS=true
 
 build_gapps=1
 
 exp_gapps () {
-export USE_GAPPS=true
+export USE_GAPPS=false
 }
 
 compile_plox () {
