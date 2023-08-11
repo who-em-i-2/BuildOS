608977d8d64059a8a790b33a441c60d35d4c21a9
NotZeetaa <rodrigo2005contente@gmail.com>
2023-08-11 12:07:04 +0000

    setup: Switch to latency branch
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b lineage kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b latency kernel
 
 # Build
 cd kernel
