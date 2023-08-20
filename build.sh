366557208ad14a31a008eb9d69821270fef3463f
ImSpiDy <SpiDy2713@gmail.com>
2023-08-21 00:48:30 +0530

    SkyLine UI
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # Normal build steps
 . build/envsetup.sh
-lunch superior_lavender-userdebug
+lunch aosp_lavender-userdebug
 
-build_gapps=1
+build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
@@ -11,7 +11,7 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
 #export WITH_GMS=false
 
-exp_gapps() {
+#exp_gapps() {
 export USE_GAPPS=true
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
@@ -20,8 +20,8 @@ export BLISS_BUILD_VARIANT=gapps
 export TARGET_USES_MINI_GAPPS=true
 export GAPPS_BUILD_TYPE=1
 export BUILD_WITH_GAPPS=true
-}
+#}
 
 compile_plox () {
-make bacon -j12
+mka bacon -j12
 }
