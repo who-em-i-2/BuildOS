16bd5d674cb7b63e54f5160ff0baad9817410914
ImSpiDy <SpiDy2713@gmail.com>
2023-10-01 05:32:47 +0530

    add support for cache
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,12 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+df -h && free -h
+
+cd /tmp
+
+# pkg
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update && apt install -y wget pigz gh jq aria2 lld git-lfs
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b f2fs-lineage kernel
+git clone --depth=1 https://github.com/ImSpiDy/kernel_xiaomi_lavender-4.19 kernel
+cd kernel
+
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 15G
+ccache -o compression=true
+ccache -z
 
 # Build
-cd kernel
-bash build.sh null alioth null
-bash build.sh null munch null
-bash build.sh null lmi null
-bash build.sh null apollo null
+bash build.sh
 
+cd /tmp
+
+tar --use-compress-program="pigz -k -0 " -cf ccache.tar.gz ccache
+
+df -h
+free -h
