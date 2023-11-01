dc4b178d6567fc58df03ed9bcf43141b9882f19c
ImSpiDy <SpiDy2713@gmail.com>
2023-11-01 20:51:13 +0530

    yet another build | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 . build/envsetup.sh
 lunch nad_lavender-user
 
+build_gapps=1
+
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
-
-build_gapps=1
+export USE_GAPPS=true
 
 exp_gapps () {
-export USE_GAPPS=true
+export USE_GAPPS=false
 }
 
 compile_plox () {
