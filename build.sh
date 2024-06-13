c8e38ecbf8c8447a50fd7c4e09a0ebcb9138aaad
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-06-13 20:07:02 +0530

    ci: lets go los20 | C
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-user
+lunch lineage_lavender-userdebug
 
 build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export PRODUCT_DEFAULT_DEV_CERTIFICATE=vendor/lineage-priv/keys/releasekey
 
 exp_gapps () {
 export USE_GAPPS=false
