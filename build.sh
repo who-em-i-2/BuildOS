e6d4dc05f50972004ddb84a8efdafebf650f83d6
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-06-13 23:39:51 +0530

    ci: less go losex
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch lineage_lavender-ap1a-user
 
 build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
-export PRODUCT_DEFAULT_DEV_CERTIFICATE=vendor/lineage-priv/keys/releasekey
+#export SELINUX_IGNORE_NEVERALLOWS=true
+export RELAX_USES_LIBRARY_CHECK=true
+if [ $K19 == 1 ]; then
+export TARGET_KERNEL_VERSION=4.19
+elif [ $K19 == 0 ]; then
+export TARGET_KERNEL_VERSION=4.4
+fi
 
 exp_gapps () {
 export USE_GAPPS=false
