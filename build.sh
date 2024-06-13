48a7f5ae1299c4846e3cc595d60edf131b4a6120
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-06-14 03:37:22 +0530

    ci: lessgo los20-4.19 | C
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-ap1a-user
+lunch lineage_lavender-userdebug
 
 build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
-#export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
 if [ $K19 == 1 ]; then
 export TARGET_KERNEL_VERSION=4.19
 elif [ $K19 == 0 ]; then
 export TARGET_KERNEL_VERSION=4.4
 fi
+export PRODUCT_DEFAULT_DEV_CERTIFICATE=vendor/lineage-priv/keys/releasekey
 
 exp_gapps () {
 export USE_GAPPS=false
