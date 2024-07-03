62afbc2d636818dcd2fe2eaaf17f0577f4f3a155
ImSpiDy <SpiDy2713@gmail.com>
2024-07-03 13:38:09 +0530

    4.19 eng build | C
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Normal build steps
 . build/envsetup.sh
-lunch nad_lavender-user
+lunch nad_lavender-userdebug
 
 build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 
 exp_gapps () {
 export USE_GAPPS=false
