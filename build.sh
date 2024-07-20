bb87fe8764816b9c8f70e931b63226f4048c57b5
ImSpiDy <SpiDy2713@gmail.com>
2024-07-20 11:28:08 +0530

    User | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 # Normal build steps
 . build/envsetup.sh
-lunch reloaded_lavender-userdebug
+lunch reloaded_lavender-user
 
 build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
+export ROM_IS_CLO=true
 
 exp_gapps () {
 export USE_GAPPS=false
