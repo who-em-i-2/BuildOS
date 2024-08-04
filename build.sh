f564b71259046ed42b5fd9952823e4aee6019f02
wHo-EM-i <ehteshammalik18998@gmail.com>
2024-08-05 01:08:35 +0530

    ci: lets go A11 4.19 | C
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Normal build steps
 . build/envsetup.sh
-lunch voltage_lavender-userdebug
+lunch lineage_lavender-userdebug
 
 build_gapps=0
 
@@ -22,17 +22,13 @@ export USE_GAPPS=false
 
 compile_plox () {
 #ls out/target/product/lavender/system.img || make systemimage -j10
-#m api-stubs-docs -j7 || tg "api-stubs-docs failed"
-#tg "Started api-stubs-docs compilation"
-m api-stubs-docs-non-updatable -j7 || tg "api-stubs-docs-non-updatable failed"
-#m system-api-stubs-docs -j7 || tg "system-api-stubs-docs failed"
-m system-api-stubs-docs-non-updatable -j7 || tg "system-api-stubs-docs-non-updatable failed"
-m test-api-stubs-docs-non-updatable -j7 || tg "test-api-stubs-docs failed"
-#m test-api-stubs-docs -j7 || tg "test-api-stubs-docs failed"
-#m module-lib-api -j7 || tg "module-lib-api failed"
+
+#m api-stubs-docs-non-updatable -j7 || tg "api-stubs-docs-non-updatable failed"
+
+#m system-api-stubs-docs-non-updatable -j7 || tg "system-api-stubs-docs-non-updatable failed"
+#m test-api-stubs-docs-non-updatable -j7 || tg "test-api-stubs-docs failed"
 #m module-lib-api-stubs-docs-non-updatable -j7 || tg "module-lib-api-stubs-docs-non-updatable failed"
-#m module-lib-api-stubs-docs -j 7 || tg "module-lib-api-stubs-docs failed"
-#m hwbinder-stubs-docs -j 7 || tg "hwbinder-stubs-docs failed"
+
 #tg "Now starting actual build"
-m bacon -j7
+m bacon -j12
 }
