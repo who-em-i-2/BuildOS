1f5899a2f90d1f822253e2b109d9443a64f6c28e
ImSpiDy <SpiDy2713@gmail.com>
2024-08-13 01:34:51 +0530

    QTI HAP | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export USE_GAPPS=false
 }
 
 compile_plox () {
-#make nad -j16
-make Settings -j16
-upload out/target/product/lavender/system/product/priv-app/Settings/Settings.apk
+make nad -j16
+#make Settings -j16
+#upload out/target/product/lavender/system/product/priv-app/Settings/Settings.apk
 }
