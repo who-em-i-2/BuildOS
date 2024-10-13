b9262e99b2b587eecc623fe5d87c87857e5c6d3b
ImSpiDy <SpiDy2713@gmail.com>
2024-10-13 14:57:11 +0530

    fixup! | R
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ lunch lineage_lavender-userdebug
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export RELAX_USES_LIBRARY_CHECK=true
 
 build_gapps=0
 export GAPPS=false
